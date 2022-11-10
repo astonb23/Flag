073ad0e1e66a10c209171a6f4482c1303af6f5ed
Aston Bonitto <116588261+astonb23@users.noreply.github.com>
2022-11-10 01:40:36 +0000

    ColorButton.java and ColorText.java

--- GUI/PaintApp/ColorButton.java
+++ GUI/PaintApp/ColorButton.java
@@ -4,16 +4,27 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.Random;
 
-public class ColorButton{
+public class ColorButton extends JButton{
     //instance variables
     private String name;
-    private  Color color;
+    private Color color;
     public static Random rand = new Random();
     //constructor
     public ColorButton(){
-    this.name = "try me";
-    this.color = new
+        name  = "Try me";
+          color = new
     Color(rand.nextInt(255),rand.nextInt(255),rand.nextInt(255));
+
+        this.setBackground(color);
+        this.setText(name);
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color g) {
+        color = g;
     }
 
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
+import GUI.PaintApp.ColorText;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
 import Game.Clicker1;
 
-class Main {
+public class Main {
     public static void main(String[] args){
-        new Clicker1();
+        new ColorText();
     }
 }
